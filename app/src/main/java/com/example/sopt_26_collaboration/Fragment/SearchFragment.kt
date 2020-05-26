90e05fe084458bb2d19ca567093b79544bdbd3ae
SEONGGYU96 <link5658@gmail.com>
2020-05-27 00:16:35 +0900

    폰트 굵기 및 gravity 수정

--- app/src/main/java/com/example/sopt_26_collaboration/Fragment/SearchFragment.kt
+++ app/src/main/java/com/example/sopt_26_collaboration/Fragment/SearchFragment.kt
@@ -29,9 +29,9 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val fieldList = mutableListOf<FieldData>()
-        fieldList.add(FieldData("개발", "#24e0bd"))
-        fieldList.add(FieldData("웹 개발자", "#428bff"))
-        fieldList.add(FieldData("자바 개발자", "#2c5bf2"))
+        fieldList.add(FieldData("서버\n개발자", "#24e0bd"))
+        fieldList.add(FieldData("웹\n개발자", "#428bff"))
+        fieldList.add(FieldData("자바\n개발자", "#2c5bf2"))
 
         val otherFieldList = mutableListOf<OtherFieldData>()
         otherFieldList.add(OtherFieldData("프론트엔드,\n이직할 때 고려할점", R.drawable.an_search_content_1_img))
